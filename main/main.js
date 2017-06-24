20ab30ab989cd80fac4913b612baf5b848fea55a
brobird <netassadove@gmail.com>
2017-06-24 19:45:53 +0800

    initial main.js

--- main/main.js
+++ main/main.js
@@ -1,6 +1,44 @@
 const loadAllItems = require('./loadAllItems.js');
 
 module.exports = function main() {
-    console.log("Debug Info");
+    var inputs = [
+        'ITEM000000',
+        'ITEM000000',
+        'ITEM000000',
+        'ITEM000000',
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000004'
+    ];
+    var item = eval(new loadAllItems());
+    var actualText = function printInventory(inputs) {
+        var textHead = '***<没钱赚商店>购物清单***\n';
+        var textBody = "";
+        var line = '----------------------\n';
+        var textEnd = '**********************';
+        var text = "";
+        var num = 0;
+        var total = 0;
+        for(var i=0;i<6;i++) {
+          while (inputs[j] == item[i][0]) {
+            for(var j=0;j<inputs.length;){
+              j++;
+            }
+            num++;
+          }
+          if(num!=0){
+            var sum = num * item[i].price;
+            toal += sum;
+            textBody += "名称：" + item[i].name + "，数量：" +
+            num + item[i].unit + "，单价：" + item[i].price +
+            "(元)，" + "小计：" + sum + "(元)\n";
+          }
+          num = 0;
+        }
+        text += textHead + textBody + line + "总价：" + total + "(元)\n" + textEnd;
+        return text;
+    };
+    console.log(actualText(inputs));
     return 'Hello World!';
 };
