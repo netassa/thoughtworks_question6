ed1946c298d6f10ad30bba76c3437163988f314b
brobird <netassadove@gmail.com>
2017-06-24 19:53:46 +0800

    update main.js 3

--- main/main.js
+++ main/main.js
@@ -11,7 +11,7 @@ module.exports = function main() {
         'ITEM000001',
         'ITEM000004'
     ];
-    var item = loadAllItems;
+    var item = new loadAllItems;
     var actualText = function printInventory(inputs) {
         var textHead = '***<没钱赚商店>购物清单***\n';
         var textBody = "";
